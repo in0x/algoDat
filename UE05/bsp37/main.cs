52c0497f685181ac7eb3551de44dabb5760b8d0f
Philipp Welsch <pwelsch.mmt-b2014@fh-salzburg.ac.at>
2015-05-06 19:01:22 +0200

    Finished bsp37

--- UE05/bsp37/main.cs
+++ UE05/bsp37/main.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 
 class MainClass {
+	
 	static void Main() {
 		string[,] matrix = {
 			{"0","0","0","x","0","x"},
@@ -15,22 +16,41 @@ class MainClass {
 		BFS labPath = new BFS(matrix);
 		labPath.mapLabyrinth(new Node(2,1,0));
 		labPath.print();
+		Console.WriteLine(labPath.buildPath(new Node(2, 5, 0)));
+
+		Console.WriteLine("Generating Test Lab");
+		Node start = labPath.genLab();
+		labPath.print();
+		
+		Console.WriteLine("Solving");
+		labPath.mapLabyrinth(start);
+		labPath.print();
+
+		Console.WriteLine("Generating Test Lab");
+		start = labPath.genLab();
+		labPath.print();
+		
+		Console.WriteLine("Solving");
+		labPath.mapLabyrinth(start);
+		labPath.print();
+
 	}
 }
 
 class BFS {
 	
-	private  Queue<Node> path;
-	private  string[,] lab;
-	
+	private Queue<Node> path;
+	private string[,] lab;
+	private Random rand;
+
 	public BFS(string[,] _lab) {
 		lab = _lab;
 		path = new Queue<Node>();
+		rand = new Random();
 	}
-	
+
 	//Helper function to check if the next node is traversable, if yes, enqueues it
 	private void traverseNeighbour(Node current, Node walk) {
-		
 		Node next = current.Add(walk);
 		if (next.x == -1 || next.y == -1 || next.x > lab.GetLength(1) - 1 || next.y > lab.GetLength(0) - 1)
 			return;
@@ -41,16 +61,13 @@ class BFS {
 		} else if (lab[next.y, next.x] == "x")
 			return;
 		return;
-		
 	}
 	
 	//Traverses the labyrinth
-	public void mapLabyrinth(Node start) {
-		
+	public void mapLabyrinth(Node start) {	
 		path.Enqueue(start);
-		bool mapped = false;
 		
-		while(!mapped) {
+		while(true) {
 			if (path.Count == 0)	
 				break;
 			
@@ -60,9 +77,16 @@ class BFS {
 			traverseNeighbour(current, new Node(0, + 1, 0));
 			traverseNeighbour(current, new Node(0,- 1, 0));	
 		}
+		
+		for (int y = 0; y < lab.GetLength(0); y++) 
+			for (int x = 0; x < lab.GetLength(1); x++) 
+				if (lab[y, x] == "0")
+					lab[y, x] = "u";
+					 
+		lab[start.y, start.x] = "0";		
 	}
 	
-	public void print() {
+	public void print() {		
 		for (int y = 0; y < lab.GetLength(0); y++) {
 			for (int x = 0; x < lab.GetLength(1); x++) {
 				Console.Write(lab[y, x] + " ");		
@@ -70,8 +94,62 @@ class BFS {
 			Console.WriteLine();
 		}
 	}
-}
 
+	private bool findNext(ref Node current, Node next) {
+		if (current.x + next.x == -1 || current.y + next.y == -1 || current.x + next.x > lab.GetLength(1) - 1 || current.y + next.y > lab.GetLength(0) - 1)
+			return false;
+		if (lab[current.y + next.y, current.x + next.x] == "x")
+			return false;
+		if (Convert.ToInt32(lab[current.y + next.y, current.x + next.x]) == Convert.ToInt32(lab[current.y, current.x]) - 1) {
+			current = current.Add(next);
+			return true;
+		}
+		else 
+			return false;
+	}
+
+	public string buildPath(Node target) {
+		string path = "";
+
+		while (Convert.ToInt32(lab[target.y, target.x]) != 0) {
+			if (findNext(ref target, new Node(1, 0, 0))) 
+				path += "u";
+			else if (findNext(ref target, new Node(-1, 0, 0)))
+				path += "d";
+			else if (findNext(ref target, new Node(0, 1, 0)))
+				path += "l";
+			else if (findNext(ref target, new Node(0, -1, 0)))
+				path += "r";
+			else {
+				path += "Target unreachable";
+				break;
+			}
+		}
+		return ReverseString(path);
+	}
+
+	public string ReverseString(string s)
+    {
+		char[] arr = s.ToCharArray();
+		Array.Reverse(arr);
+		return new string(arr);
+    }
+	
+	public Node genLab() {	
+		for (int y = 0; y < lab.GetLength(0); y++) {
+			for (int x = 0; x < lab.GetLength(1); x++) {
+				if (rand.Next(11) > 2) 
+					lab[y, x] = "0";
+				else 
+					lab[y, x] = "x";
+			}
+		}
+
+		Node start = new Node(rand.Next(6), rand.Next(6), 0);
+		lab[start.y, start.x] = "*";
+		return start;
+	}
+}
 
 struct Node {
 	public int y;
